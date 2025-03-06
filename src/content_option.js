01c53573c7c607dcfafd565d8e833659f1541751
amangirdhar474 <amangirdhar474@gmail.com>
2025-03-06 23:57:06 +0530

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "I love to explore new technologies",
     },
     description: "Dynamic B. Tech graduate in Computer Science with expertise in software development and database management. Proficient in Java, Python, and SQL. Effective communicator with strong teamwork and leadership abilities, eager to contribute technical proficiency and collaborative skills to a progressive IT team.",
-    your_img_url: "https://drive.google.com/file/d/1sxpph3yp_ar0T-VFJKnz6CKbsKuohzx3/view?usp=drivesdk",
+    your_img_url: "https://unsplash.com/photos/MrW2WwF9X-Q",
 };
 
 const dataabout = {
